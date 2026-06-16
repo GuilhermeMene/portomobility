f93755578839f9589346261cb4dbe343f80174c4
Guilherme Primo <guilherme.ale.primo@live.com>
2026-06-16 19:00:02 +0100

    UPDATE the set env

--- code/set_env.sh
+++ code/set_env.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 #
+# Check if the argments are passed
+if [ $# -eq 0 ]; then
+    echo "No arguments are passed." >&2
+    exit 1
+fi
+
+#Check if the argument is a string
+if [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Error: Argument '$1' is an integer, expected a text string." >&2
+    exit 1
+fi
+
 # Set the env variables
 set -a
-. ./.env
+. $1
 set +a
+
+#Check if it's worked
+if [ $? -eq 0 ]; then
+    echo "The environment was sucessful saved."
+else
+    echo "The environment cannot be saved."
+fi
