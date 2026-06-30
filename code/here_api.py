f591ff5e3cd35c2ad49dd82fb0a5cc820da8c90d
Guilherme Primo <guilherme.ale.primo@live.com>
2026-06-30 07:54:15 +0100

    UPDATE HERE api

--- code/here_api.py
+++ code/here_api.py
@@ -28,7 +28,6 @@ def fetch_here_data(output_path, here_key, amount_file):
         timestamp = int(time.time())
         filename = os.path.join(output_path, TYPE, f"{TYPE}_{timestamp}.json")
 
-        print(response)
         if response.status_code == 200:
             json_data = response.json()
             with open(filename, "w", encoding="utf-8") as f:
